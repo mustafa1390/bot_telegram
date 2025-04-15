fbff051a73ce00588ffecbd916c0c4b05d4942ea
mostafa yosefi <51215135+mustafa1390@users.noreply.github.com>
2025-04-15 13:31:07 +0330

    a7

--- app/Services/Telegram/BotTelegram.php
+++ app/Services/Telegram/BotTelegram.php
@@ -366,7 +366,7 @@ $payload = [
 
 
                     $bot_status = BotStatus::where([ ['id','=',1],   ])->update( ['registerdone' => 0 ] );
-                    $text_html = " 🎴 چند تصویری وجود دارد! 🎴  
+                    $text_html = " 🎴 چند تصویری وجود دارد! 🎴  {$caption}
 
 {$fileName}";
                     $data = [
